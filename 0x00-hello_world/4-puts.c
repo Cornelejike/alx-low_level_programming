c5b83bb26c81eb9368ab0dcf0a9d2eb51f200d93
Cornelejike <cornelejike@gmail.com>
2023-10-05 18:04:22 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <dtdio.h>
-/**
+ #include <dtdio.h>
+ /**
  * main - A program that prints a line with puts function
  * Return: 0 (success)
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
-}
-
+ int main(void)
+ {
+ 	 puts("\"Programming is like building a multilingual puzzle");
+	 Return (0);
+ } 

2c1a45fad28417498cee3417d15bb00d1d74ad8f
Cornelius Onuh <cornelejike@gmail.com>
2024-07-08 07:58:29 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string source
+ * @c: the char to locate
+ *
+ * Return: string located
+ */
+char *_strchr(char *s, char c)
+{
+	int m = 0, n;
+
+	while (s[m])
+		m++;
+	for (n = 0; n < m + 1; n++)
+	{
+		if (c == s[n])
+		{
+			s += n;
+			return (s);
+		}
+	}
+	return ('\0');
+}
